ecb04b8893915fdff6168d14ee35fdef9ebaf26d
Davi <davi.cesarbt@gmail.com>
2024-09-06 16:20:47 -0300

    update na estilização do container e dp name

--- src/app/components/contact/styles.ts
+++ src/app/components/contact/styles.ts
@@ -3,8 +3,15 @@ import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
     container: {
+        width: "100%",
+        flexDirection:"row",
+        alignItems: "center",
+        borderRadius: 7,
+        gap:7,
     },
     name:{
-        
+        color:theme.colors.black,
+        fontFamily: theme.fontFamily.medium,
+        fontSize: 18,
     }
 })
