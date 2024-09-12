36ae20b3c0f02247cd7a32f6d70e26954d3156f2
Davi <davi.cesarbt@gmail.com>
2024-09-12 11:01:27 -0300

    atualização na imagem dos contatos

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,13 +1,36 @@
-import { View } from 'react-native'
+import { Alert, View } from 'react-native'
 import { styles } from './styles'
 import { Input } from '@/app/components/input'
 import { Feather } from '@expo/vector-icons'
 import { theme } from "@/themes"
-import { useState } from 'react'
-import { Contact } from '@/app/components/contact'
+
+import * as Contacts from 'expo-contacts'
+import { useState, useEffect } from 'react'
+import { Contact } from '../components/contact'
+
+/**/
+async function fetchContacts(){
+    try{
+        const { status } = await Contacts.requestPermissionsAsync()
+        if(status === Contacts.PermissionStatus.GRANTED){
+            const { data } = await Contacts.getContactsAsync()
+            console.log(data)
+        }
+    } catch(error){
+        console.log(error)
+        Alert.alert("Contatos", "Não foi possível carregar os contatos...")
+    }
+}
+
+
+/**/
 
 export function Home(){
     const [name, setName] = useState("")
+
+    useEffect(() => {
+        fetchContacts()
+    }, [])
 
     return(
         <View style={styles.container}>
@@ -20,7 +43,7 @@ export function Home(){
             </View>
             <Contact contact={{
                 name: "Conor",
-                image: require("@assets/avatar.jpeg")
+                image: require("@/assets/avatar.jpeg")
             }}/>
         </View>
     )
